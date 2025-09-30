dfadfd89a5050c73954824df81534ee7c8389cbf
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-09-30 20:32:54 +0530

    operators

--- operators.js
+++ operators.js
@@ -1,8 +1,11 @@
 // comparision operator
-var a = "2"
-var b = 2
-
+var a = "10"
+var b = 5
+var a1="heLLo"
+var s1="HEllo"
 console.log(a==b)
 console.log(a++)
 console.log(++b)
 console.log(a === b)
+console.log(a1 == s1)
+// console.log(a1 != s1)
