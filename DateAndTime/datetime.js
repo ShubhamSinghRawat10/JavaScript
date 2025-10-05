5af02d26ffed0349c57eca5c3e89d64d2720d344
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-10-05 14:38:44 +0530

    Date and Time

---
+++ DateAndTime/datetime.js
@@ -0,0 +1,11 @@
+let datetoday = new Date();
+console.log(datetoday);
+console.log(typeof datetoday);
+console.log(datetoday.toString());
+console.log(datetoday.toISOString());
+console.log(datetoday.toLocaleString());
+console.log(datetoday.toDateString());
+
+let datetoday1 = new Date(2025, 9,5);
+console.log(datetoday1.toLocaleString());
+console.log(datetoday1.toDateString());
