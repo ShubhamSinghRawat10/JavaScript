901a597a27facbb2780314f04eb34322544017e0
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-10-22 02:45:30 +0530

    Basics of Functions in JS

---
+++ Functions/Basics.js
@@ -0,0 +1,12 @@
+//! we will learn how to create functions in javascript
+
+// function declaration
+function addtwonumber(num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+// function call
+let sum = addtwonumber(5, 10);
+console.log("SUM", sum); // Output: Sum of two numbers is: 15
+
+// ?addtwonumbers --> THIS IS ONLY THE REFERAL OF THE FUNCTION , MEANS IT TELLS THAT THE FUNCTION EXIST HERE.. 
