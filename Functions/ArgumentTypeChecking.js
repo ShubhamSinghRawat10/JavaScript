a27865cc2bff407b2f19b3f59359e47511e84942
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-11-01 17:02:03 +0530

    Type checking in Arguments

--- Functions/ArgumentTypeChecking.js
+++ Functions/ArgumentTypeChecking.js
@@ -1,7 +1,7 @@
 obj = {
     "name": "Shubham",
     "age": 24,
-    "course-subjects": ["HTML", "CSS", "JS", "ReactJS"],
+    "subjects": ["HTML", "CSS", "JS", "ReactJS"],
 };
 obj1 =10;
 b=30;
@@ -10,9 +10,9 @@ function printval(o,s){
     // Specially when we are working with functions in js we to keep constraint on type checking.
 
     console.log(typeof o);
-    if(typeof 0=== "Object"){
+    if(typeof o === "object"){
         console.log(o.name);
-        console.log(o.course-subjects[2]);
+        console.log(o.subjects[2]);
     }
     else {
         console.log("Not an object");
