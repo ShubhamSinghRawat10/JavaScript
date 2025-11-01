1fc14c272b0af93ba64c642a666babce9fd634cd
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-11-01 21:44:06 +0530

    New array type checking

--- Functions/ArgumentTypeChecking.js
+++ Functions/ArgumentTypeChecking.js
@@ -1,23 +1,55 @@
-obj = {
-    "name": "Shubham",
-    "age": 24,
-    "subjects": ["HTML", "CSS", "JS", "ReactJS"],
-};
-obj1 =10;
-b=30;
+// obj = {
+//     "name": "Shubham",
+//     "age": 24,
+//     "subjects": ["HTML", "CSS", "JS", "ReactJS"],
+// };
+// obj1 =10;
+// b=30;
 
-function printval(o,s){
+// function printval(o,s){
     // Specially when we are working with functions in js we to keep constraint on type checking.
 
-    console.log(typeof o);
-    if(typeof o === "object"){
-        console.log(o.name);
-        console.log(o.subjects[2]);
-    }
-    else {
-        console.log("Not an object");
+//     console.log(typeof o);
+//     if(typeof o === "object"){
+//         console.log(o.name);
+//         console.log(o.subjects[2]);
+//     }
+//     else {
+//         console.log("Not an object");
+//     }
+// }
+// printval(obj);
+// printval(obj1); 
+// printval(b);
+
+
+//!
+
+
+ a1=[10,20,30,40,50];
+ a=10;
+
+ function Arraypaas(a){
+    if(Array.isArray(a)){
+        ob = {
+            rollno: arr,
+        };
+        console.log(ob.rollno);
+    }else {
+        console.log("Not an Array" );
     }
 }
-printval(obj);
-printval(obj1); 
-printval(b);
+
+function objectprint(arr){
+    if (typeof arr === "object"){
+        ob= {
+            ...arr,
+        };
+        console.log(ob);
+    } else {
+        console.log("Not an object");
+    }
+ }
+
+ objectprint(a1,a);
+
