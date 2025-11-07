c4dfbc63f9144fede87bf3561ad4b052b546a756
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-11-07 19:31:02 +0530

    Fat arrow function

--- FunctionWithObjects/FatArrow.js
+++ FunctionWithObjects/FatArrow.js
@@ -1,9 +1,14 @@
 //! Function Referencing 
 
-// var printhello = function heloo() {
-//     console.log("Hello World");
-// };
-// printhello();
+var printhello = function heloo() {
+    console.log("Hello World");
+};
+printhello();
+
+
+
+
+//! Fat Arrow Function --> ES6 (ECMA Script 6)
 
 // 1. Fat Arrow Function is always defined using const keyword
 // 2. In Fat Arrow Function, the value of 'this' is lexically inherited from the outer function where it is defined.
@@ -12,17 +17,21 @@
 //=> FAt Arrow Function Syntax
 // function referencing to a variable using const keyword
 
-// const printHello = () => {
-//     console.log("Hello World ");
-// };
-// printhello();
+const printHello = () => {
+    console.log("Hello World ");
+};
+printhello();
 
 // Example 2: Fat Arrow Function with parameters
 
-// const add = (a,b) => {
-//     console.log("Heloo Shubham ");
-//     return a + b;
-// };
+const add = (a,b) => {
+    console.log("Heloo Shubham ");
+    return a + b;
+};
 
-const add = (a, b) => a + b; // Implicit return
-console.log(add(5, 10));
+
+//! if you are not giving any other value in the function other than return then you can remove {} and return keyword also
+//? One liner fat arrow function
+
+// const add = (a, b) => a + b; // Implicit return
+// console.log(add(5, 10));
