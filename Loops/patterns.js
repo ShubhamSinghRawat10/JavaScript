461db1382f8a13622fbb15b88f86e0f4b46007f0
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-11-16 18:49:01 +0530

    Patterns question

---
+++ Loops/patterns.js
@@ -0,0 +1,29 @@
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+let n = 7;
+
+for (let i = 1; i <= n; i=i+2) {
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write("* ");
+    }
+    console.log(" ");
+}
+
+
+//? let n=5;
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+for ( let i=n ; i>=1; i-- ) {
+    for ( let j=1 ; j<=i ; j++ ) {
+        process.stdout.write("* ");
+    }
+    console.log();
+}
+
