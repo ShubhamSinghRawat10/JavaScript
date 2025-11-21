60294cab810145465f912100acd7a71f12de813a
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-11-21 19:26:17 +0530

    For each loop with Fall through

--- ArrayLoop/ForEach.js
+++ ArrayLoop/ForEach.js
@@ -19,5 +19,58 @@ function sum(val) {
 a1.forEach(sum);
 console.log(add);
 
-// reference function functionname
+//! reference function functionname
 // function calling sum(arugment)
+
+
+a1 = ["apple", "mango", "Banana", "Kiwi", "Strawberry"];
+
+// console.log(
+//   a1.forEach((value) => {
+//     console.log(value);
+//     return value;
+//   })
+// );
+
+a1.forEach((value) => {
+  // console.log(value);
+});
+
+// forEach-->for every element of the array it will iterate once itself and pass the value of the array to the callback function
+// ob = {
+//   name: "Karan",
+//   Course: "Btech",
+//   College: "BIAS",
+// };
+
+// Object.keys(ob).forEach((vv) => {
+//   // console.log(vv); to print keys using object
+// });
+// Object.values(ob).forEach((vv) => {
+//   // console.log(vv); to print values using object
+// });
+// Object.entries(ob).forEach(([a, b], c) => {
+//   console.log(${a}---->${b}-->${c});
+// });
+
+// // BREAK   -->FALL THROUGH
+// // continue
+
+// MAP();
+// FILTER();
+// REDUCE();
+
+//
+
+char = "a";
+switch (char) {
+  case "a":
+  case "i":
+  case "e":
+  case "o":
+  case "u":
+    return "Vowel";
+    break;
+  default:
+    return "Consonant";
+}
