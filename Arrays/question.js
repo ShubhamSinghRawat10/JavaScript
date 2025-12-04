521a49cf3b6c28357970dcb394bae43fa9fae4fc
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-12-04 18:57:59 +0530

    another method

--- Arrays/question.js
+++ Arrays/question.js
@@ -1,11 +1,28 @@
 //! write a function to check if the user object has name, email and age properties. If all properties are present, return 1 else return 0.
 //! functon as arguemnt
-function checkuserdata(user) {
-    const { name, age, email } = user;
+// function checkuserdata(user) {
+//     const { name, age, email } = user;
+
+//     return (name && email && age) ? 1 : 0;
+// }
+
+// const user = {
+//     name: "shubham",
+//     email: "shubh.rwt2005@gmail.com",
+//     age: 18,
+// };
+
+// console.log(checkuserdata(user));  // Output: 1
+
+
+
+//! another method using destructuring in function parameter
+
+function checkuserdata({ name, age, email }) {
+    // const { name, age, email } = user;
 
     return (name && email && age) ? 1 : 0;
 }
-
 const user = {
     name: "shubham",
     email: "shubh.rwt2005@gmail.com",
