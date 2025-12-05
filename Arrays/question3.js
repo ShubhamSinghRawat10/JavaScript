c83c19057f864e2abcafd7e8502fd2caa9c4b088
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-12-05 20:55:40 +0530

    question 3 using one liner function

---
+++ Arrays/question3.js
@@ -0,0 +1,18 @@
+// Name must have minimum 3 characters,
+// Valid email must contain "@" and ".com",
+// Age must be 18+
+//! oneliner version of validateUser function
+function validateUser({ name, age, email }) {
+    
+ return ( (namevalid = typeof name === "string" && name.length >= 3) && (agevalid = typeof age === "number" && age >= 18) && (typeof email === "string" 
+                       && email.includes("@") 
+                       && email.includes(".com")));
+}
+
+const user = {
+    name : "Ali",
+    email : "shubhamsinghrawat2005@gmail.com",
+    age : 20
+};
+
+console.log(validateUser(user)); // true
