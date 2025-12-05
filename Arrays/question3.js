e9fad4095e355eaad5fb97891eaaaebc1e2fe92a
Shubham Singh rawat <shubhamsinghrawat01@gmail.com>
2025-12-05 20:57:45 +0530

    solving Q3 using fat arrow function

--- Arrays/question3.js
+++ Arrays/question3.js
@@ -2,13 +2,20 @@
 // Valid email must contain "@" and ".com",
 // Age must be 18+
 //! oneliner version of validateUser function
-function validateUser({ name, age, email }) {
+// function validateUser({ name, age, email }) {
     
- return ( (namevalid = typeof name === "string" && name.length >= 3) && (agevalid = typeof age === "number" && age >= 18) && (typeof email === "string" 
-                       && email.includes("@") 
-                       && email.includes(".com")));
-}
+//  return ( (namevalid = typeof name === "string" && name.length >= 3) && (agevalid = typeof age === "number" && age >= 18) && (typeof email === "string" 
+//                        && email.includes("@") 
+//                        && email.includes(".com")));
+// }
 
+
+//! using Fat arrow function
+const validateUser = ({ name, age, email }) =>
+    ( (namevalid = typeof name === "string" && name.length >= 3) && (agevalid = typeof age === "number" && age >= 18) && (typeof email === "string"
+                          && email.includes("@")
+                            && email.includes(".com")));
+                            
 const user = {
     name : "Ali",
     email : "shubhamsinghrawat2005@gmail.com",
